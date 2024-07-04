2807f2c284af22cc3a455f388543c0d57ba13179
ZakyG05 <ahmadzaqy99@gmail.com>
2024-07-04 19:25:30 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    String nama = "Lexy";
+    int umur = 10;
+    String warna = "coklat dan krem";
+    String tinggi = "90-120 cm";
+    String berat = "60-100 kg";
+    String makanan = "1. Daging, 2. Ular, 3. Hiu";
+
+    public static void main(String[] args) {
+        Main test = new Main();
+        System.out.println("Nama: " + test.nama);
+        System.out.println("Umur: " + test.umur);
+        System.out.println("Warna: " + test.warna);
+        System.out.println("Tinggi: " + test.tinggi);
+        System.out.println("Berat: " + test.berat);
+        System.out.println("Makanan: " + test.makanan);
+    }
+}
